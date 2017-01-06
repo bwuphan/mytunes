bb58ae4b9523f3ac6232189f7512b6853ff31e9d
bobby <bwuphan@gmail.com>
2017-01-05 19:47:41 -0800

    finished tests except parse and libary

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -19,7 +19,11 @@ var AppModel = Backbone.Model.extend({
 
     params.library.on('enqueue', function(song) {
       this.get('songQueue').add(song);
-    }, this)
+    }, this);
+
+    params.library.on('stop', function(){
+      this.set('currentSong', null);
+    });
   }
 
 });
